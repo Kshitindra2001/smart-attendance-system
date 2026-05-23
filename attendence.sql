154c93fc53fe5ed639340c6c42a32f723df5558b
Kshitindra2001 <kshitindra1997@gmail.com>
2026-05-23 16:10:26 +0530

    Add files via upload

---
+++ attendence.sql
@@ -0,0 +1,27 @@
+-- drop database if not exists db1;
+-- drop database if not exists db2;
+-- show databases;
+-- use db1;
+-- create table employee(
+-- empid int primary key,
+-- name varchar(255),
+-- age int,
+-- salary decimal (9,2)
+-- );
+-- drop table employee;
+
+-- show tables;
+-- show databases;
+-- use db1; 
+-- create table employee1(
+--  empid int primary key,
+--  name varchar(255),
+--  age int not null,
+--  salary int,
+--  department_name varchar(100)
+-- );
+show tables;
+
+
+
+
